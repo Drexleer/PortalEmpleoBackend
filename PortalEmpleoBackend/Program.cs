218eb91c6589b504464679657a6688b5c139e389
Edder Jose Alvarez Rodriguez <drexleer01@gmail.com>
2024-04-23 09:43:34 -0500

    Implementando Swagger

--- PortalEmpleoBackend/Program.cs
+++ PortalEmpleoBackend/Program.cs
@@ -4,13 +4,22 @@ using PortalEmpleoBackend.Models;
 using PortalEmpleoDB;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<PortalEmpleoDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Para que la ruta raíz (/) redirija a Swagger
+});
 
 //using (var scope = app.Services.CreateScope())
 //{
@@ -30,7 +39,9 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "swagger",
+    pattern: "swagger",
+    defaults: new { controller = "Swagger", action = "Index" }
+);
 
 app.Run();
